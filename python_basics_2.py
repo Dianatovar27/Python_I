ba5856ffdbf08a9c36a6272849cb21ecc548cc2d
Diana Tovar Reolon <dianatovarreolon@gmail.com>
2024-10-19 21:27:40 -0500

    deleted old files

--- python_basics_2.py
+++
@@ -1,9 +0,0 @@
-print( 'How many words would you like to add to your list (minimum 7)?', end = ' ')
-listnumber = int(input())
-list=[]
-print('Plese give me ', listnumber, ' words:')
-
-for w in range(listnumber):
-    w= input()
-    list.append (w)
-print(list)
