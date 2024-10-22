743a105c2c0ae001ca8734ed544be16652c26fe6
Diana Tovar Reolon <dianatovarreolon@gmail.com>
2024-10-21 19:52:41 -0500

    cleaned

--- Python_basics/python_basics_2.py
+++ Python_basics/python_basics_2.py
@@ -1,53 +1,53 @@
-#print( 'How many words would you like to add to your list (minimum 7)?', end = ' ')
-#listnumber = int(input())
-#list=[]
-#print('Plese give me', listnumber, 'words: ')
+print( 'How many words would you like to add to your list (minimum 7)?', end = ' ')
+listnumber = int(input())
+list=[]
+print('Plese give me', listnumber, 'words: ')
 
-#for w in range(listnumber):
-    #w= input()
-    #list.append (w)
-#print(list)
+for w in range(listnumber):
+    w= input()
+    list.append (w)
+print(list)
 
-# Se pide un numero entre 1 y 7, más una palabra. El numero introducido indica la posición en la lista anterior cuyo valor será sustituido por la palabra introducida.
 
-#numb = int(input('give me a number between 1 and 7: '))
-#nw= input('give me a new word: ')
 
-#list[numb] = nw
-#print(list)
+numb = int(input('give me a number between 1 and 7: '))
+nw= input('give me a new word: ')
+
+list[numb] = nw
+print(list)
 
 # Se pide la posición de un elemento en la lista y una vez introducida se elimina dicha palabra.
 
-#delete= int(input( 'which position betweeen 1 and 7 would you like to delete? '))
+delete= int(input( 'which position betweeen 1 and 7 would you like to delete? '))
 
-#del list[delete]
+del list[delete]
 
-# Crea un diccionario de 10 palabras ingles-español y realiza 3 consultas a este diccionario
+ Crea un diccionario de 10 palabras ingles-español y realiza 3 consultas a este diccionario
 
-#dictranslate= {'dog': 'perro', 'cat': 'gato', 'table' : 'mesa', 'car': 'carro', 'girl' : 'nina', 'boy' : 'nino', 'sun' : 'sol', 'moon':'luna', 'pencil': 'lapiz', 'flower': 'flor' }
+dictranslate= {'dog': 'perro', 'cat': 'gato', 'table' : 'mesa', 'car': 'carro', 'girl' : 'nina', 'boy' : 'nino', 'sun' : 'sol', 'moon':'luna', 'pencil': 'lapiz', 'flower': 'flor' }
 
-#for i in range(3):
-    #ingles= input('which word would you like to check? ')
-    #print(dictranslate[ingles])
+for i in range(3):
+    ingles= input('which word would you like to check? ')
+    print(dictranslate[ingles])
 
-#dictranslate.update({'house': 'casa'})
+dictranslate.update({'house': 'casa'})
 
-#print(dictranslate)
+print(dictranslate)
 
 # Introduce una lista de 10 palabras y muéstralas de la ultima a la primera
 
-#nlist=[]
+nlist=[]
 
-#for i in range(10):
-    #nlist.append(input())
-#print(nlist)
+for i in range(10):
+    nlist.append(input())
+print(nlist)
 
-#contador = 9
-#for w in range(10):
-    #print(nlist[contador])
-    #contador = contador-1
+contador = 9
+for w in range(10):
+    print(nlist[contador])
+    contador = contador-1
 
-# Mediante funciones realiza un programa que simule una calculadora
+Mediante funciones realiza un programa que simule una calculadora
 
 
 def menu():
