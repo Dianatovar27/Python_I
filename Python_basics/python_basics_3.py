1f40b91307c0f66c4505cfe6e3686823095e3636
Diana Tovar Reolon <dianatovarreolon@gmail.com>
2024-10-23 20:16:07 -0500

    created a module using random library, updated an excel sheet using openpyxl, graph creation using matplotlib

---
+++ Python_basics/python_basics_3.py
@@ -0,0 +1,42 @@
+import random2
+import openpyxl
+import matplotlib.pyplot as graph
+
+# Utilizando el modulo anterior obtén 30 notas aleatorias y guardaras en Excel
+
+#random_list=[]
+
+#for n in range(30):
+    #random_list.append(random2.givemenumber(1,10))
+#print(random_list)
+
+excel = openpyxl.load_workbook('C:\\Users\\jfdr1\\Desktop\\Python_I\\Python_basics\\random_numbers.xlsx')
+
+sheet = excel['numbers']
+
+#sheet.append(random_list)
+
+#excel.save('C:\\Users\\jfdr1\\Desktop\\Python_I\\Python_basics\\random_numbers.xlsx')
+
+# Crea un grafico que muestre si hay mas aprobados o suspensos
+
+grade_card= sheet['A1':'AD1']
+
+passed = 0
+failed = 0
+
+for row in grade_card:
+    for grade in row:
+        if grade.value >= 5:
+            passed = passed + 1
+        else:
+            failed = failed + 1
+
+x= ['pass', 'fail']
+y = [passed, failed]
+
+print("There are " + str(passed) + ' students who passed and  ' + str(failed) + ' students who failed.')
+
+graph.bar(x, y, color=['blue', 'red'])
+
+graph.show()
---
+++ Python_basics/random2.py
@@ -0,0 +1,4 @@
+import random
+
+def givemenumber(min, max):
+    return random.randint(min, max)
